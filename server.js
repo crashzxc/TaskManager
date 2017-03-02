f1d29f432b7852c9e8aeb121827261e691a80f88
KyJack <jackkau12@hotmail.com>
2017-03-02 11:41:51 +0800

    1. First push

---
+++ server.js
@@ -0,0 +1,118 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express = require('express')
+    , cors = require('cors')
+    , app = express();//https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
+
+app.use(cors());
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var TaskItem     = require('./app/models/TaskItem');
+var firebase = require('firebase');
+var moment = require('moment');
+var https = require('https');
+var request = require('request');
+moment = require('moment-timezone');
+require('moment-duration-format');
+
+// SETUP FIREBASE
+// =============================================================================
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBiueePlYHApAZO7uK1jBpxUq9V-OljSas",
+    authDomain: "mytaskmanager-62ffa.firebaseapp.com",
+    databaseURL: "https://mytaskmanager-62ffa.firebaseio.com",
+    storageBucket: "mytaskmanager-62ffa.appspot.com",
+    messagingSenderId: "935287859446"
+};
+firebase.initializeApp(config);
+var databaseReference = firebase.database();
+var locationsDbRef = databaseReference.ref('tasks');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());//Enable cors for all routes
+var port = process.env.PORT || 3000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+router.route('/TaskManager/addTaskItem')
+// create a Location (accessed at POST http://localhost:8080/api/TaskManager/addTaskItem)
+    .post(function(req, res) {
+        // create a new instance of the Location model
+        var location = new TaskItem(locationsDbRef,req.body.taskName);
+        // save the bear and check for errors
+        location.saveToFirebase(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Task Item data created in Firebase' });
+        });
+
+    });
+
+
+router.route('/TaskManager/getTask')
+    .get(function(req, res) {
+        var ref = firebase.database().ref('tasks');
+
+        ref.on('value', function(item) {
+                //console.log('child_added', item.val());
+                res.json({ message:item.val() });
+
+            },
+            function (error) {
+                console.log("Error: " + error.code);
+            });
+    });
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/TaskManager/testingonly', function(req, res) {
+//
+//     // create a new instance of the Location model
+//     var taskItem = new TaskItem(locationsDbRef,'omg');
+//     // save the bear and check for errors
+//     taskItem.saveToFirebase(function(err) {
+//         if (err)
+//             res.send(err);
+//         res.json({ message: 'Task Item data created in Firebase' });
+//     });
+//
+// });
+
+
+
+
+
+// router.get('/TaskManager', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });
+//
+// });
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
